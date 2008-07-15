8abc1e53230f8c21acd2850e246b27c652c3023c
armadilo <armadilo@a6e6f860-6479-40d1-8235-aed7a033c797>
2008-07-15 10:21:19 +0000

    Converted from regular script to class and methods to be invoked in client application. Added functionality to url-ify some of the free-form parameters and pack all of them into a single URL/URI to be sent via GET.

--- client/tunneler_client.py
+++ client/tunneler_client.py
@@ -1,40 +1,56 @@
 import sys
 import os
+import socket
+import re
 from urllib2 import *
 
-baseurl="http://amat.inveneo.org/reg?"
+
+class amat_client (object):
+
+	baseurl="http://amat.inveneo.org/reg?"
 """
 https://<reg server>/reg?mac=<mac>&type=<host type>&host=<hostname>&cust=<customer>&desc=<short description>&geo=<geocode>&opperiod=<opperiod> 
 """
+	def __init__(self,parent):
 
-mac="00145135cb20" 
-type="client"
-fullcust="somerandomcustname"
-fulldesc="some free random descriptive text"
-cust=fullcust[0:99]
-desc=fulldesc[0:299]
-geo="-37.784825,-122.419968"
+		mac="00145135cb20" 
+		type="client"
+		fullcust="somerandomcustname"
+		fulldesc="some free random descriptive text"
+		cust=fullcust[0:99]
+		desc=fulldesc[0:299]
+		geo="-37.784825,-122.419968"
 
-if os.path.exists("/etc/hostname"):
-	host=os.popen('cat /etc/hostname').read()
-else:
-	hn=os.popen('which hostname').read()
-	if hn.length(hn) > 0:
-		host=hn[0:49]
-	else:
-		host="inveneo"+type
+	def gethostname(self):
+		if os.path.exists("/etc/hostname"):
+			host=os.popen('cat /etc/hostname').read()
+		else:
+			hn=os.popen('which hostname').read()
+			if hn.length(hn) > 0:
+				host=hn[0:49]
+			else:
+				host="inveneo"+type
 
-fullurl=baseurl+"mac="+mac+"&type="+type+"&host="+host+"&cust="+cust+"&desc="+desc+"&geo="+geo+"&opperiod=12345:0900-1700,06:1200-1800"
+	def encode_text(self,parent,intext):
+		outtext=''
+		outtext=urllib2.quote(intext)
+	        return outtext	
 
-print "Initial parameters are:"
-print "mac "+mac
-print "hosttype "+type
-print "hostname "+host
-print "cust "+cust
-print "desc "+desc
-print "geo "+geo
+	def build_url(self,mac,type,host,cust,desc,geo,opperiod):
+		enc_desc=''
+		enc_desc=self.encode_text(desc) 
+		fullurl=baseurl+"mac="+mac+"&type="+type+"&host="+host+"&cust="+cust+"&desc="+desc+"&geo="+geo+"&opperiod=12345:0900-1700,06:1200-1800"
 
-print "Full URL="+fullurl
+	def dumpparms(self):
+
+		print "Initial parameters are:"
+		print "mac "+mac
+		print "hosttype "+type
+		print "hostname "+host
+		print "cust "+cust
+		print "desc "+desc
+		print "geo "+geo
+		print "Full URL="+fullurl
 
 #
 #req = Request('http://twitter.com/users/show/jetdillo.json')
