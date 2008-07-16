b739ea9fedee84bd88b9a0ead5e04d76e24bc9eb
armadilo <armadilo@a6e6f860-6479-40d1-8235-aed7a033c797>
2008-07-16 22:43:27 +0000

    Added code to modularize url-sending. There is now a separate builder/concatenator for register and checkin URLs

--- client/amat_client.py
+++ client/amat_client.py
@@ -12,7 +12,7 @@ class amc (object):
 
 	def __init__(self,parent):
 
-		self.baseurl="http://bb.inveneo.net:5000/reg?"
+		self.baseurl="http://bb.inveneo.net:5000/"
 
 	def gethostname(self):
 		if os.path.exists("/etc/hostname"):
@@ -20,20 +20,28 @@ class amc (object):
 			host=host.strip()		
 		else:
 			hn=os.popen('which hostname').read()
-			if hn.length(hn) > 0:
+			if len(hn) > 0:
 				host=hn[0:49]
 			else:
 				host="inveneo"+type
 		return host
 
-	def build_url(self,mac,type,host,cust,desc,geo,opperiod):
+	def build_reg_url(self,mac,type,host,cust,desc,geo,opperiod):
 		trim_desc=desc[0:299]
 #		enc_desc=self.encode_text(trim_desc) 
 		geo = geo.replace(',','%2C')		
 		opperiod = opperiod.replace(',','%2C')
 		opperiod = opperiod.replace(':','%3A')
-		fullurl=self.baseurl+"mac="+mac+"&type="+type+"&host="+host+"&cust="+cust+"&desc="+desc+"&geo="+geo+"&opperiod="+opperiod
-		return fullurl
+		reg_url=self.baseurl+"reg?"+"mac="+mac+"&type="+type+"&host="+host+"&cust="+cust+"&desc="+desc+"&geo="+geo+"&opperiod="+opperiod
+		return reg_url
+
+	def build_checkin_url(self,mac,status):
+		results=0	
+		print "mac="+mac+" status="+status
+		print "mac length=%d status length=%d",len(mac),len(status)
+		mac = mac[0:11]
+		checkin_url=self.baseurl+"checkin?"+"mac="+mac+"&status="+status
+		return checkin_url
 
 	def dumpparms(self):
 
@@ -47,14 +55,20 @@ class amc (object):
 		print "Full URL="+fullurl
 
 	def connect(self,url):
-	
+		req=''
+		resp=''	
 		try:
-#			req=Request(url)
-			req=urlopen(url)
+			req=Request(url)
+			resp=urlopen(req)
 
 		except URLError, e:
 			print "caught exception ",e," trying to open "+url
 			sys.exit(1)	
 
-		results = req.read()
+		results = resp.read()
 		print results 
+
+
+	def parse_results(self,resp_data):
+
+		print "IM IN YR RESPONSE ]<0d3, SHR3DD1n yr DATA!!"
