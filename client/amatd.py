647486d9077f5182c7385cc5185b6974499116c6
jwiggins <jwiggins@a6e6f860-6479-40d1-8235-aed7a033c797>
2008-10-16 00:22:21 +0000

    daemonizer works

--- client/amatd.py
+++ client/amatd.py
@@ -3,4 +3,31 @@
 # amatd.py
 # (c) Inveneo 2008
 
+import os, daemonize
+
+if __name__ == "__main__":
+
+    retCode = daemonize.createDaemon(os.getcwd())
+
+    procParams = """
+    return code = %s
+    process ID = %s
+    parent process ID = %s
+    process group ID = %s
+    session ID = %s
+    user ID = %s
+    effective user ID = %s
+    real group ID = %s
+    effective group ID = %s
+    """ % (retCode, os.getpid(), os.getppid(), os.getpgrp(), os.getsid(0),
+    os.getuid(), os.geteuid(), os.getgid(), os.getegid())
+
+    open("createDaemon.log", "w").write(procParams + "\n")
+
+    # uncomment this if you want daemon to stay alive
+    import time
+    while 1:
+        time.sleep(1)
+
+    sys.exit(retCode)
 
